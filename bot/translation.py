c35fc0e1c471984e27a559391dde386fc0bec774
Disneyteam <87754364+Disneyteam77@users.noreply.github.com>
2021-11-27 12:36:53 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -3,54 +3,41 @@
 # (c) @AlbertEinsteinTG
 
 class Translation(object):
+
+    START_IMG = """https://telegra.ph/file/1f1e5ba848b569cfb366e.jpg"""
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b>Hey</b>
+<i>HOW ARE YOU {} DUDE 🤩,Nice To Meet You🙌
+
+𝖨'𝗆 𝗃𝗎𝗌𝗍 𝖺 𝗌𝗂𝗆𝗉𝗅𝖾 𝗉𝗋𝖾 - 𝖿𝗎𝗇𝖼𝗍𝗂𝗈𝗇𝖾𝖽 𝖺𝗎𝗍𝗈𝖿𝗂𝗅𝗍𝖾𝗋 𝖻𝗈𝗍
+Its Simple To Use Me..☺️,Just Add Me To Your Group As Admin,Thats All I Will Provide Movies There..🥰
 
 Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
 For More Details Click Help Button Below..
-@disneygrou 
+@disneygrou
 </i>"""    
     
     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
+<b><i><u>HEY {} DUDE 🙋‍♂️
 
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
+Just Add To Your Group As Admin Its All.☺️, 
+I Will Provide Movies There...⚡️</u></i>
 
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-@disneygrou
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/doreamonfans1">doreamonfans1</a></i></b>
+    ABOUT_TEXT = """<b>➥  IAM JUST A  Auto-Filter-Bot 🚶‍♂️⚠️
 
-<b>➥ Language</b> : <code>Python3</code>
+<b>➪ 𝑴𝒚 𝒏𝒂𝒎𝒆 : RExiE
+<b>➪ 𝑪𝒓𝒆𝒂𝒕𝒐𝒓 : DoraemonFans1 url="https://t.me/doreamonfans1" 🕵️‍♂️
+<b>➪ 𝑪𝒓𝒆𝒅𝒊𝒕𝒔 : Everyone in this journey
+<b>➪ 𝑳𝒊𝒃𝒓𝒂𝒓𝒚 : PYROGRAM📜
+<b>➪ 𝑳𝒂𝒏𝒈𝒖𝒂𝒈𝒆 : Phyton 3
+<b>➪ 𝑫𝒂𝒕𝒂 𝒃𝒂𝒔𝒆 : MANGO DB
+<b>➪ 𝑩𝒐𝒕 𝒔𝒆𝒓𝒗𝒆𝒓 : Heroku
+<b>➪ 𝑺𝒐𝒖𝒓𝒄𝒆 𝑪𝒐𝒅𝒆 : 👉 COMING SOON
+<b>➪ 𝑩𝒖𝒊𝒍𝒅 𝑺𝒕𝒂𝒕𝒖𝒔 : v1.0.1 [ BETA  ]
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+
+🔖 𝑸𝒖𝒐𝒕𝒆 : ആരും പേടിക്കേണ്ട എല്ലാവർക്കും കിട്ടും
 """
